6f03e35bb92c18305c477a4b4b384ea28b46e614
Sanguzsua <sanguzsua20@gmail.com>
2025-05-09 19:51:10 +0000

    Configuración de PWA con manifest y service worker

---
+++ vistas/service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/trivia.css",
+        "/script.js",
+        "/manifest.json",
+        "imagenes/icono.png",
+        "imagenes/splash.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
